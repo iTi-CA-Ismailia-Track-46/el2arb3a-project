59bc9c1a97f4cb511c6679738e24fd4ecfe8ff9a
kofta999 <mostafaxxx555@gmail.com>
2026-06-08 11:40:24 +0300

    fix: add missing subnet cidrs

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -25,11 +25,11 @@ variable "vpc_cidr" {
 variable "public_subnets" {
   description = "Public subnets CIDRs"
   type        = list(string)
-  default     = ["10.0.1.0/24"]
+  default     = ["10.0.1.0/24", "10.0.3.0/24"]
 }
 
 variable "private_subnets" {
   description = "Private subnets CIDRs"
   type        = list(string)
-  default     = ["10.0.2.0/24"]
+  default     = ["10.0.2.0/24", "10.0.4.0/24"]
 }
--- Terraform/vpc.tf
+++ Terraform/vpc.tf
@@ -10,6 +10,8 @@ module "vpc" {
   cidr = var.vpc_cidr
 
   azs             = slice(data.aws_availability_zones.available.names, 0, 2)
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
 
   enable_nat_gateway = true
   single_nat_gateway = true

90d0e188947311e871ed967d7674b3b2ba46f235
Adham Ayad <aboayad557@gmail.com>
2026-06-08 10:47:14 +0300

    feat: Add Terraform infrastructure provisioning files and configurations

---
+++ Terraform/provider.tf
@@ -0,0 +1,24 @@
+terraform {
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      Project     = "KubeVision-Serverless-Platform"
+    }
+  }
+}
